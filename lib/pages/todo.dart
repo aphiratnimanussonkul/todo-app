5c9d936332c6eb30dc3325b61a446dfe46dc8eab
Aphirat Nimanussonkul <b5923151@gmail.com>
2023-06-02 09:39:03 +0700

    [Todo] refactor

--- lib/pages/todo.dart
+++ lib/pages/todo.dart
@@ -12,52 +12,61 @@ class _TodoPageState extends State<TodoPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-          width: MediaQuery.sizeOf(context).width,
-          padding: const EdgeInsets.fromLTRB(32, 64, 32, 32),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "My Todo",
-                    style: TextStyle(
-                      color: Color(0xff252D89),
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    "you have 3 tasks remain for today",
-                    style: TextStyle(
-                      color: Color(0xff858CA7),
-                      fontSize: 14,
-                    ),
-                    textAlign: TextAlign.center,
-                  )
-                ],
-              ),
-              ElevatedButton(
-                style: const ButtonStyle(
-                  backgroundColor: MaterialStatePropertyAll<Color>(
-                    Color(0xff3E4ADE),
-                  ),
-                ),
-                child: Container(
-                  padding: const EdgeInsets.symmetric(vertical: 12),
-                  width: MediaQuery.sizeOf(context).width,
-                  child: const Text(
-                    "Add Todo",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-                onPressed: () => Navigator.pushNamed(context, "/todo"),
-              ),
-            ],
-          )),
+        width: MediaQuery.sizeOf(context).width,
+        padding: const EdgeInsets.fromLTRB(32, 64, 32, 32),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            title(),
+            addTodoButton(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Column title() {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "My Todo",
+          style: TextStyle(
+            color: Color(0xff252D89),
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          "you have 3 tasks remain for today",
+          style: TextStyle(
+            color: Color(0xff858CA7),
+            fontSize: 14,
+          ),
+          textAlign: TextAlign.center,
+        )
+      ],
+    );
+  }
+
+  ElevatedButton addTodoButton(BuildContext context) {
+    return ElevatedButton(
+      style: const ButtonStyle(
+        backgroundColor: MaterialStatePropertyAll<Color>(
+          Color(0xff3E4ADE),
+        ),
+      ),
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 12),
+        width: MediaQuery.sizeOf(context).width,
+        child: const Text(
+          "Add Todo",
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      onPressed: () => Navigator.pushNamed(context, "/todo"),
     );
   }
 }
